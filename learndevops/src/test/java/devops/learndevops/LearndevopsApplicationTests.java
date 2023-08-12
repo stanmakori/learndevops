7f06ce8b808e0f4a12e1a54f27cdc9b4a8d5c83a
stanmakori <stanmakori@gmail.com>
2023-08-12 18:34:00 +0300

    Initial commit

---
+++ learndevops/src/test/java/devops/learndevops/LearndevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package devops.learndevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearndevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/main/java/devops/learndevops/LearndevopsApplication.java
@@ -0,0 +1,13 @@
+package devops.learndevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearndevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearndevopsApplication.class, args);
+	}
+
+}
